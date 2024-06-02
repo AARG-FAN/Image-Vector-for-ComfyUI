d4ab21e33b1b81434cb23690bb18b3d2bcd2938b
AARG-AI FAN <125116261+archifancy@users.noreply.github.com>
2024-06-02 12:09:48 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .AIvector import AIvector
+
+NODE_CLASS_MAPPINGS = {
+    "AIvector": AIvector
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "AIvector": "AIvector"
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
